8495e4798a829b7131f2fd0d5eda42649185f7f1
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-03-01 17:21:56 -0300

    Adding userRegister endpoint

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ const CORS = require("cors");
 server.listen(3000, () => console.log("Server Started"));
 
 server.use(bodyParser.json(), CORS());
+
+server.post("/v1/users/", registerUser, (req, res) => {
+  const { isCreated } = req;
+  isCreated
+    ? res.status(201).json("User Created")
+    : res.status(400).json("Missing Arguments");
+});
+
+function registerUser(req, res, next) {
+  const { userName, name, password, email, address, phone } = req.body;
+  if (userName && name && password && email && address && phone) {
+    req.isCreated = true;
+    next();
+  } else {
+    req.isCreated = false;
+    next();
+  }
+  //logica de la base de datos
+}
