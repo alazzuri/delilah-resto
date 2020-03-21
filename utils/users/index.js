f3788eb96fe54b857c759a310f3936eea0726ac9
Alexis Lazzuri <56130546+alazzuri@users.noreply.github.com>
2020-03-21 12:27:46 -0300

    users refactoring

--- utils/users/index.js
+++ utils/users/index.js
@@ -1,40 +1,29 @@
-const { sequelize, insertQuery, selectQuery } = require("../../db");
+//DATABASE
+const { insertQuery, selectQuery, sequelize } = require("../../db");
 
-async function findUserByName(req) {
-  const { firstname, lastname } = req.body;
-  const userExists = async () => {
-    const query = selectQuery(
-      "users",
-      "firstname, lastname",
-      `firstname = '${firstname}' AND lastname = '${lastname}'`
-    );
-    const [dbUser] = await sequelize.query(query, { raw: true });
-    const existingUser = await dbUser.find(
-      element =>
-        element.firstname === firstname && element.lastname === lastname
-    );
+async function findUserByName(firstname, lastname) {
+  const query = selectQuery(
+    "users",
+    "firstname, lastname",
+    `firstname = '${firstname}' AND lastname = '${lastname}'`
+  );
+  const [dbUser] = await sequelize.query(query, { raw: true });
+  const existingUser = await dbUser.find(
+    element => element.firstname === firstname && element.lastname === lastname
+  );
 
-    return existingUser ? true : false;
-  };
-  return await userExists();
+  return existingUser ? true : false;
 }
 
-async function validateExistingUser(req, res, next) {
-  try {
-    const existingUser = await findUserByName(req);
-    if (!existingUser) {
-      const dbUsers = await findUserbyUsername(req.body.username);
-      if (!dbUsers) {
-        next();
-      } else {
-        res.status(409).json("Username already in use");
-      }
-    } else {
-      res.status(409).json("User already exists");
-    }
-  } catch (err) {
-    next(new Error(err));
-  }
+async function findUserByUsername(username) {
+  const query = selectQuery(
+    "users",
+    "user_id, username, password, is_admin",
+    `username = '${username}'`
+  );
+  const [dbUser] = await sequelize.query(query, { raw: true });
+  const foundUser = dbUser[0];
+  return foundUser;
 }
 
 async function registerUser(req, res, next) {
@@ -83,24 +72,28 @@ async function registerUser(req, res, next) {
   }
 }
 
-async function findUserbyUsername(username) {
-  const existingUser = async () => {
-    const query = selectQuery(
-      "users",
-      "user_id, username, password, is_admin",
-      `username = '${username}'`
-    );
-    const [dbUser] = await sequelize.query(query, { raw: true });
-    const foundUser = dbUser[0];
-    return foundUser;
-  };
-
-  return existingUser();
+async function validateExistingUser(req, res, next) {
+  const { firstname, lastname, username } = req.body;
+  try {
+    const existingUser = await findUserByName(firstname, lastname);
+    if (!existingUser) {
+      const dbUsers = await findUserByUsername(username);
+      if (!dbUsers) {
+        next();
+      } else {
+        res.status(409).json("Username already in use");
+      }
+    } else {
+      res.status(409).json("User already exists");
+    }
+  } catch (err) {
+    next(new Error(err));
+  }
 }
 
 module.exports = {
   findUserByName,
-  findUserbyUsername,
+  findUserByUsername,
   registerUser,
   validateExistingUser
 };
